c4b95f96a1ab340f12ff8771104afe5b48e99957
Dominique St-Amand <126111771+dstamand-msft@users.noreply.github.com>
2025-06-02 15:37:22 -0400

    Fix formatting issue

--- Scripts/DownloadArtifacts.ps1
+++ Scripts/DownloadArtifacts.ps1
@@ -103,7 +103,7 @@ foreach ($item in $filesToDownload) {
         # If the blobName is a wildcard, download all blobs in the container
         if ($blobName.EndsWith("*")) {
             if ($blobName.Length -eq 1 -and $blobName -eq "*") {
-             $blobs = Get-AzStorageBlob -Container $containerName -Context $storageAccountContext
+                $blobs = Get-AzStorageBlob -Container $containerName -Context $storageAccountContext
             }
             else {
                 # blob supports wildcard (*) search, so we need to get all blobs that match the pattern
